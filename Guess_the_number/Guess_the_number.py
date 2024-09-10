0aed9b67dfb6fdd99ffb466edc53f1578ddd9709
Valentin <valentin.tsekhmystro@gmail.com>
2024-09-10 20:03:42 +0300

    Guess_the_number

--- Guess_the_number/Guess_the_number.py
+++ Guess_the_number/Guess_the_number.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import random
+
+number = random.randint(0, 100)
+
+print("Випадкове число вибране спробуй його відгадати!")
+
+while True:
+    number_people = (int(input("Напиши число:")))
+    if number_people < number:
+        print("Спробуй більше число!")
+    elif number_people > number:
+        print("Спробуй менше число!")
+    elif number_people == number:
+        print("Молодець ти перміг!!!")
+        break
